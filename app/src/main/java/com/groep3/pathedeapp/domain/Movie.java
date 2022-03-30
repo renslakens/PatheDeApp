1f45e0af96c61744667d2e2d5252ecc7ba82da5c
Niek <Niekelodius@gmail.com>
2022-03-30 18:55:08 +0200

    snel een soort van werkende recycler view gemaakt

--- app/src/main/java/com/groep3/pathedeapp/domain/Movie.java
+++ app/src/main/java/com/groep3/pathedeapp/domain/Movie.java
@@ -3,9 +3,9 @@ package com.groep3.pathedeapp.domain;
 public class Movie {
     public boolean adult;
     public String backdropPath;
-    public BelongsToCollection belongsToCollection;
+//    public BelongsToCollection belongsToCollection;
     public int budget;
-    public List<Genre> genres = null;
+//    public List<Genre> genres = null;
     public String homepage;
     public int id;
     public String imdbId;
@@ -14,16 +14,27 @@ public class Movie {
     public String overview;
     public float popularity;
     public String posterPath;
-    public List<ProductionCompany> productionCompanies = null;
-    public List<ProductionCountry> productionCountries = null;
+//    public List<ProductionCompany> productionCompanies = null;
+//    public List<ProductionCountry> productionCountries = null;
     public String releaseDate;
     public int revenue;
     public int runtime;
-    public List<SpokenLanguage> spokenLanguages = null;
+//    public List<SpokenLanguage> spokenLanguages = null;
     public String status;
     public String tagline;
     public String title;
     public boolean video;
     public float voteAverage;
     public int voteCount;
+
+    public Movie(String name) {
+        //main info
+        this.title = name;
+
+    }
+
+
+    public String getName() {
+        return title;
+    }
 }
