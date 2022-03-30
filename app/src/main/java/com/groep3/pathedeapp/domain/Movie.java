9cc7f0d4c0d2225994437e66d29265a17386a964
Rens Lakens <rens@lakens.org>
2022-03-30 18:02:29 +0200

    added Movie in domain package

---
+++ app/src/main/java/com/groep3/pathedeapp/domain/Movie.java
@@ -0,0 +1,29 @@
+package com.groep3.pathedeapp.domain;
+
+public class Movie {
+    public boolean adult;
+    public String backdropPath;
+    public BelongsToCollection belongsToCollection;
+    public int budget;
+    public List<Genre> genres = null;
+    public String homepage;
+    public int id;
+    public String imdbId;
+    public String originalLanguage;
+    public String originalTitle;
+    public String overview;
+    public float popularity;
+    public String posterPath;
+    public List<ProductionCompany> productionCompanies = null;
+    public List<ProductionCountry> productionCountries = null;
+    public String releaseDate;
+    public int revenue;
+    public int runtime;
+    public List<SpokenLanguage> spokenLanguages = null;
+    public String status;
+    public String tagline;
+    public String title;
+    public boolean video;
+    public float voteAverage;
+    public int voteCount;
+}
