0710420bb98f7bc69f026279815d2500edfe4d20
Rens Lakens <rens@lakens.org>
2022-03-31 11:32:10 +0200

    changed api base url

--- app/src/main/java/com/groep3/pathedeapp/dataacces/NetworkUtils.java
+++ app/src/main/java/com/groep3/pathedeapp/dataacces/NetworkUtils.java
@@ -15,7 +15,7 @@ public class NetworkUtils {
         URL url;
         String inline = "";
         try {
-            url = new URL("https://api.themoviedb.org/3/discover/movie?api_key=bce3e84f67721f9e61473a5c397a0bf1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate");
+            url = new URL("https://api.themoviedb.org/3/discover/movie?api_key=bce3e84f67721f9e61473a5c397a0bf1");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             Log.i(TAG, "Connecting to API");
