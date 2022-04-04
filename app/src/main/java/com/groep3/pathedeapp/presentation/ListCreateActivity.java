6ea442ea92e54bd8e26643d18a8401314a6e07f4
Rens Lakens <rens@lakens.org>
2022-04-04 18:31:29 +0200

    created list activity with xml file

---
+++ app/src/main/java/com/groep3/pathedeapp/presentation/ListCreateActivity.java
@@ -0,0 +1,16 @@
+package com.groep3.pathedeapp.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.groep3.pathedeapp.R;
+
+public class ListCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_create);
+    }
+}
