fdacd3948adc2fcdb614aa5caccce06dfa5e43ea
Rens Lakens <rens@lakens.org>
2022-04-04 19:07:20 +0200

    onClick bij create list gezet

--- app/src/main/java/com/groep3/pathedeapp/presentation/ListCreateActivity.java
+++ app/src/main/java/com/groep3/pathedeapp/presentation/ListCreateActivity.java
@@ -2,7 +2,9 @@ package com.groep3.pathedeapp.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.groep3.pathedeapp.R;
 
@@ -12,5 +14,10 @@ public class ListCreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_create);
+    }
+
+    public void createList(View view) {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
