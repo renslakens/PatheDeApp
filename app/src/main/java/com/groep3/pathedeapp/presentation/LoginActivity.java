09b0d9583b92107bc8a56027c500021a1352baad
Rens Lakens <rens@lakens.org>
2022-04-04 16:46:37 +0200

    changed login screen

--- app/src/main/java/com/groep3/pathedeapp/presentation/LoginActivity.java
+++ app/src/main/java/com/groep3/pathedeapp/presentation/LoginActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -35,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
         getRequestToken();
