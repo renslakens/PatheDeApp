a0ebef4dcf0cb5271a95d3f6e5eb56614ba079f2
Rens Lakens <rens@lakens.org>
2022-04-04 18:42:06 +0200

    R class geïmporteerd

--- app/src/main/java/com/groep3/pathedeapp/presentation/ListActivity.java
+++ app/src/main/java/com/groep3/pathedeapp/presentation/ListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.groep3.pathedeapp.R;
+
 public class ListActivity extends AppCompatActivity {
 
     @Override
