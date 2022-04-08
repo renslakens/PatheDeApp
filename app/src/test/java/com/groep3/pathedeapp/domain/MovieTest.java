33374c0228523e17dfd25cdf6bd5177bbaee066d
Rens Lakens <rens@lakens.org>
2022-04-08 20:36:38 +0200

    goede junit test library toegevoegd

--- app/src/test/java/com/groep3/pathedeapp/domain/MovieTest.java
+++ app/src/test/java/com/groep3/pathedeapp/domain/MovieTest.java
@@ -1,8 +1,8 @@
 package com.groep3.pathedeapp.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class MovieTest {
 
