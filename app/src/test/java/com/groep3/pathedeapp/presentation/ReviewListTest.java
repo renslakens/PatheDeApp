9bacecbf4673bbd97fb4cee80540875774d05fe8
codeSTACKr <codeSTACKr@gmail.com>
2022-04-08 19:24:02 +0200

    added unit test file ReviewListTest.java

---
+++ app/src/test/java/com/groep3/pathedeapp/presentation/ReviewListTest.java
@@ -0,0 +1,16 @@
+package com.groep3.pathedeapp.presentation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReviewListTest {
+
+    @Test
+    void setReviewList() {
+    }
+
+    @Test
+    void getItemCount() {
+    }
+}
