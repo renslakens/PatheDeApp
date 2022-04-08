b6d2b05b6f64274f3de7a7e31ba24bd7e9c362a0
Rens Lakens <rens@lakens.org>
2022-04-08 20:45:15 +0200

    unit test namen veranderd

--- app/src/test/java/com/groep3/pathedeapp/domain/MovieTest.java
+++ app/src/test/java/com/groep3/pathedeapp/domain/MovieTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 public class MovieTest {
 
     @Test
-    public void MovieVoteAverage_dividedBy2_1(){
+    public void MovieVoteAverageDividedBy2_1(){
         Movie movie = new Movie();
         movie.setVoteAverage(6.4);
 
         assertEquals(movie.getVoteAverage() / 2, 3.2, 0.01);
     }
     @Test
-    public void MovieVoteAverage_dividedBy2_2(){
+    public void MovieVoteAverageDividedBy2_2(){
         Movie movie = new Movie();
         movie.setVoteAverage(9.0);
 
